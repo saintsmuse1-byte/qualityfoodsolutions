f138feb67123726aa539ff395e7ece3ed8323c1c
saintsmuse1-byte <saintsmuse1@gmail.com>
2026-07-14 14:40:54 +0200

    Refactor services page layout and content
    Updated page title and meta tags for services page. Removed headquarters address and TeleportHQ sign-up link.

--- src/views/services.js
+++ src/views/services.js
@@ -11,10 +11,10 @@ const Services = (props) => {
   return (
     <div className="services-container1">
       <Helmet>
-        <title>Services - Circular Self Reliant Sheep</title>
+        <title>Services</title>
         <meta
           property="og:title"
-          content="Services - Circular Self Reliant Sheep"
+          content="Services"
         />
         <link
           rel="canonical"
@@ -722,39 +722,6 @@ const Services = (props) => {
             </div>
           </div>
           <div className="contact-column">
-            <h3 className="section-subtitle">Our Headquarters</h3>
-            <p className="section-content">
-              <span>
-                {' '}
-                1224 Logistics Parkway
-                <span
-                  dangerouslySetInnerHTML={{
-                    __html: ' ',
-                  }}
-                />
-              </span>
-              <br></br>
-              <span>
-                {' '}
-                Industrial Hub, State 45001
-                <span
-                  dangerouslySetInnerHTML={{
-                    __html: ' ',
-                  }}
-                />
-              </span>
-              <br></br>
-              <span>
-                {' '}
-                Supply Chain Center
-                <span
-                  dangerouslySetInnerHTML={{
-                    __html: ' ',
-                  }}
-                />
-              </span>
-            </p>
-          </div>
           <div className="contact-column">
             <div className="cta-box">
               <h3 className="section-subtitle">Start Your Solution</h3>
@@ -765,7 +732,7 @@ const Services = (props) => {
                 <button id="openQuoteModal" className="btn btn-accent btn-lg">
                   Request a Quote
                 </button>
-                <a href="mailto:logistics@qualityfoodsolutions.com?subject=">
+                <a href="mailto:james@qualityfoodsolutions.co.za?subject=">
                   <div className="btn btn-lg btn-primary">
                     <span>Email Us</span>
                   </div>
@@ -890,32 +857,6 @@ const Services = (props) => {
         </div>
       </div>
       <Footer></Footer>
-      <a href="https://play.teleporthq.io/signup">
-        <div aria-label="Sign up to TeleportHQ" className="services-container4">
-          <svg
-            width="24"
-            height="24"
-            viewBox="0 0 19 21"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            className="services-icon77"
-          >
-            <path
-              d="M9.1017 4.64355H2.17867C0.711684 4.64355 -0.477539 5.79975 -0.477539 7.22599V13.9567C-0.477539 15.3829 0.711684 16.5391 2.17867 16.5391H9.1017C10.5687 16.5391 11.7579 15.3829 11.7579 13.9567V7.22599C11.7579 5.79975 10.5687 4.64355 9.1017 4.64355Z"
-              fill="#B23ADE"
-            ></path>
-            <path
-              d="M10.9733 12.7878C14.4208 12.7878 17.2156 10.0706 17.2156 6.71886C17.2156 3.3671 14.4208 0.649963 10.9733 0.649963C7.52573 0.649963 4.73096 3.3671 4.73096 6.71886C4.73096 10.0706 7.52573 12.7878 10.9733 12.7878Z"
-              fill="#FF5C5C"
-            ></path>
-            <path
-              d="M17.7373 13.3654C19.1497 14.1588 19.1497 15.4634 17.7373 16.2493L10.0865 20.5387C8.67402 21.332 7.51855 20.6836 7.51855 19.0968V10.5141C7.51855 8.92916 8.67402 8.2807 10.0865 9.07221L17.7373 13.3654Z"
-              fill="#2874DE"
-            ></path>
-          </svg>
-          <span className="services-text26">Built in TeleportHQ</span>
-        </div>
-      </a>
     </div>
   )
 }
