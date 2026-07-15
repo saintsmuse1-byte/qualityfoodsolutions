9b8abf67524de13710676517647641ccbb048da2
saintsmuse1-byte <saintsmuse1@gmail.com>
2026-07-15 14:38:39 +0200

    Modify consultant route paths in index.js
    Updated consultant route paths to new URLs for James and Fleur.

--- src/index.js
+++ src/index.js
@@ -1,39 +1,76 @@
 import React from 'react'
+
 import ReactDOM from 'react-dom'
+
 import {
+
   BrowserRouter as Router,
+
   Route,
+
   Switch,
+
   Redirect,
+
 } from 'react-router-dom'
 
+
+
 import './style.css'
+
 import Home from './views/home'
+
 import Services from './views/services'
+
 import NotFound from './views/not-found'
 
+
+
 // --- 1. IMPORTED NEW CONSULTANT PAGES ---
+
 import JamesProfile from './views/james'
+
 import FleurProfile from './views/fleur'
 
+
+
 const App = () => {
+
   return (
+
     <Router>
+
       <Switch>
+
         <Route component={Home} exact path="/" />
+
         <Route component={Services} exact path="/services" />
+
         
-        {/* --- 2. UPDATED CONSULTANT ROUTE PATHS --- */}
-        {/* These now match the <Link to="..."> paths in your Home.js exactly */}
-        <Route component={JamesProfile} exact path="/james" />
-        <Route component={FleurProfile} exact path="/fleur" />
+
+        {/* --- 2. ADDED NEW CONSULTANT ROUTE PATHS --- */}
+
+        <Route component={JamesProfile} exact path="/consultant-james" />
+
+        <Route component={FleurProfile} exact path="/consultant-fleur" />
+
         
+
         {/* Keep these fallback catch-alls at the very bottom of the Switch list */}
+
         <Route component={NotFound} path="**" />
+
         <Redirect to="**" />
+
       </Switch>
+
     </Router>
+
   )
+
 }
 
-ReactDOM.render(<App />, document.getElementById('app'))
+
+
+ReactDOM.render(<App />, document.getElementById('app')) 
+
