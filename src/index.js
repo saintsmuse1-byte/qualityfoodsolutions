9f6c9ce01495d4affd043ff584ddb0af0902564e
saintsmuse1-byte <saintsmuse1@gmail.com>
2026-07-15 12:16:51 +0200

    Add new consultant profile routes

--- src/index.js
+++ src/index.js
@@ -12,12 +12,22 @@ import Home from './views/home'
 import Services from './views/services'
 import NotFound from './views/not-found'
 
+// --- 1. IMPORTED NEW CONSULTANT PAGES ---
+import JamesProfile from './views/james'
+import FleurProfile from './views/fleur'
+
 const App = () => {
   return (
     <Router>
       <Switch>
         <Route component={Home} exact path="/" />
         <Route component={Services} exact path="/services" />
+        
+        {/* --- 2. ADDED NEW CONSULTANT ROUTE PATHS --- */}
+        <Route component={JamesProfile} exact path="/consultant-james" />
+        <Route component={FleurProfile} exact path="/consultant-fleur" />
+        
+        {/* Keep these fallback catch-alls at the very bottom of the Switch list */}
         <Route component={NotFound} path="**" />
         <Redirect to="**" />
       </Switch>
